699425f336f8757ffd144fdb51df03c0a464f0c9
nachomcapella <nachetemc97@gmail.com>
2020-04-13 14:19:52 +0200

    Dead analysis is now functional!

--- app.R
+++ app.R
@@ -13,6 +13,8 @@ library('rsconnect')
 
 source("util.R")
 
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(navbarPage(
   "COVID-19 Spain",
@@ -21,15 +23,15 @@ ui <- fluidPage(navbarPage(
              sidebarPanel(
                h3("Choose a visualization"),
                checkboxInput(
-                 "check1","Total cases (linear)", value= T),
+                 "check_sick_1","Total cases (linear)", value= T),
                checkboxInput(
-                 "check2","Total cases (log)", value= F),
+                 "check_sick_2","Total cases (log)", value= F),
                checkboxInput(
-                 "check3","New cases (absolute)", value= F),
+                 "check_sick_3","New cases (absolute)", value= F),
                checkboxInput(
-                 "check4","New cases (%)", value= F),
+                 "check_sick_4","New cases (%)", value= F),
                sliderInput(
-                 "dates",
+                 "dates_sick",
                  h3("Choose a date range"),
                  min = as.Date("2020-02-25", "%Y-%m-%d"),
                  max = as.Date("2020-04-12", "%Y-%m-%d"),
@@ -40,15 +42,35 @@ ui <- fluidPage(navbarPage(
              
              # Show a plot of the generated distribution
              mainPanel(
-               plotlyOutput("plot_sick"),
-               # plotlyOutput("plot_total_log"),
-               # plotlyOutput("plot_new_cases_abs"),
-               # plotlyOutput("plot_new_cases_perc")
-               
+               plotlyOutput("plot_sick")
              )
            )),
   tabPanel("Dead",
-           h2("I am an empty panel!")),
+           sidebarLayout(
+             sidebarPanel(
+               h3("Choose a visualization"),
+               checkboxInput(
+                 "check_dead_1","Total cases (linear)", value= T),
+               checkboxInput(
+                 "check_dead_2","Total cases (log)", value= F),
+               checkboxInput(
+                 "check_dead_3","New cases (absolute)", value= F),
+               checkboxInput(
+                 "check_dead_4","New cases (%)", value= F),
+               sliderInput(
+                 "dates_dead",
+                 h3("Choose a date range"),
+                 min = as.Date("2020-02-25", "%Y-%m-%d"),
+                 max = as.Date("2020-04-12", "%Y-%m-%d"),
+                 value = c(as.Date("2020-02-25"), as.Date("2020-04-12")),
+                 timeFormat = "%Y-%m-%d"
+               )
+             ),
+             
+             # Show a plot of the generated distribution
+             mainPanel(
+               plotlyOutput("plot_dead")
+             ))),
   tabPanel("Regions",
            h2("I am an empty panel!"))
 ))
@@ -60,22 +82,22 @@ server <- function(input, output) {
   library("ggpubr")
   
   #Reading the data:
+  print("Reading the data...")
   data <- read.csv(file = "./data/nacional_covid19.csv")
   colnames(data)[1] <- "fecha"
   data$fecha <- as.Date(data$fecha)
   data$fallecimientos[is.na(data$fallecimientos)] <- 0
+  print("Data modified!")
   
-  
-  
-  #Contagiados:
-  plot_total_linear_func <- function(dataset, title, xlab, ylab) {
-    date_range <- c(input$dates[1], input$dates[2])
+  #Creating the plots:
+  plot_total_linear_func <- function(date_range, dataset, title, ylab, xlab) {
+    
     dates <-
-      dataset$dates[dataset$dates >= input$dates[1] &
-                   dataset$dates <= input$dates[2]]
+      dataset$dates[dataset$dates >= date_range[1] &
+                   dataset$dates <= date_range[2]]
     cases <-
-      dataset$values[dataset$dates >= input$dates[1] &
-                   dataset$dates <= input$dates[2]]
+      dataset$values[dataset$dates >= date_range[1] &
+                   dataset$dates <= date_range[2]]
     date <- as.character.Date(dates)
     df <- data.frame(cases, date)
     plot_total <-
@@ -88,14 +110,13 @@ server <- function(input, output) {
     return(plot_total)
   }
   
-  plot_total_log_func <- function(dataset, title, xlab, ylab) {
-    date_range <- c(input$dates[1], input$dates[2])
+  plot_total_log_func <- function(date_range,dataset, title, ylab, xlab) {
     dates <-
-      dataset$dates[dataset$dates >= input$dates[1] &
-                      dataset$dates <= input$dates[2]]
+      dataset$dates[dataset$dates >= date_range[1] &
+                      dataset$dates <= date_range[2]]
     cases <-
-      dataset$values[dataset$dates >= input$dates[1] &
-                       dataset$dates <= input$dates[2]]
+      dataset$values[dataset$dates >= date_range[1] &
+                       dataset$dates <= date_range[2]]
     date <- as.character.Date(dates)
     df <- data.frame(cases, date)
     plot_total <-
@@ -103,21 +124,20 @@ server <- function(input, output) {
       geom_line(color = "red") +
       geom_point() +
       scale_y_continuous(trans = 'log10') +
-      ggtitle("Total cases vs date") +
-      ylab("Cases (log)") +
-      xlab("Date") +
+      ggtitle(title) +
+      ylab(ylab) +
+      xlab(xlab) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
     return(plot_total)
   }
   
-  plot_new_cases_abs_func <- function(dataset, title, xlab, ylab) {
-    date_range <- c(input$dates[1], input$dates[2])
+  plot_new_cases_abs_func <- function(date_range,dataset, title, ylab, xlab) {
     dates <-
-      dataset$dates[dataset$dates >= input$dates[1] &
-                      dataset$dates <= input$dates[2]]
+      dataset$dates[dataset$dates >= date_range[1] &
+                      dataset$dates <= date_range[2]]
     cases <-
-      dataset$values[dataset$dates >= input$dates[1] &
-                       dataset$dates <= input$dates[2]]
+      dataset$values[dataset$dates >= date_range[1] &
+                       dataset$dates <= date_range[2]]
     cases <- get_daily_increment_absolute(cases)
     date <- as.character.Date(dates)
     df <- data.frame(cases, date)
@@ -126,21 +146,20 @@ server <- function(input, output) {
              aes(x = date, y = cases, group = 1)) +
       geom_line(color = "green") +
       geom_point() +
-      ggtitle("New cases vs date") +
-      ylab("New cases (absolute)") +
-      xlab("Date") +
+      ggtitle(title) +
+      ylab(ylab) +
+      xlab(xlab) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
     return(plot_new_cases_abs)
   }
   
-  plot_new_cases_perc_func <- function(dataset, title, xlab, ylab) {
-    date_range <- c(input$dates[1], input$dates[2])
+  plot_new_cases_perc_func <- function(date_range,dataset, title, ylab, xlab) {
     dates <-
-      dataset$dates[dataset$dates >= input$dates[1] &
-                      dataset$dates <= input$dates[2]]
+      dataset$dates[dataset$dates >= date_range[1] &
+                      dataset$dates <= date_range[2]]
     cases <-
-      dataset$values[dataset$dates >= input$dates[1] &
-                       dataset$dates <= input$dates[2]]
+      dataset$values[dataset$dates >= date_range[1] &
+                       dataset$dates <= date_range[2]]
     cases <- get_daily_increment_percentage(cases)
     date <- as.character.Date(dates)
     df <- data.frame(cases, date)
@@ -150,94 +169,63 @@ server <- function(input, output) {
       geom_line(color = "blue") +
       geom_point() +
       geom_hline(yintercept = 0, color = "red") +
-      ggtitle("New cases vs date") +
-      ylab("New cases (+%)") +
-      xlab("Date") +
+      ggtitle(title) +
+      ylab(ylab) +
+      xlab(xlab) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
     return(plot_new_cases_perc)
   }
   
-  # output$plot_total_linear <- renderPlotly({
-  #   options <- input$checkGroup
-  #   if ('1' %in% options) {
-  #     plot_final <- (ggplotly(plot_total_linear_func()))
-  #   } else{
-  #     plot_final = NULL
-  #   }
-  #   return(plot_final)
-  # })
-  # output$plot_total_log <- renderPlotly({
-  #   options <- input$checkGroup
-  #   if ('2' %in% options) {
-  #     plot_final <- (ggplotly(plot_total_log_func()))
-  #   } else{
-  #     plot_final = NULL
-  #   }
-  #   return(plot_final)
-  # })
-  # output$plot_new_cases_abs <- renderPlotly({
-  #   options <- input$checkGroup
-  #   if ('3' %in% options) {
-  #     plot_final <- (ggplotly(plot_new_cases_abs_func()))
-  #   } else{
-  #     plot_final = NULL
-  #   }
-  #   return(plot_final)
-  # })
-  # output$plot_new_cases_perc <- renderPlotly({
-  #   options <- input$checkGroup
-  #   if ('4' %in% options) {
-  #     plot_final <- (ggplotly(plot_new_cases_perc_func()))
-  #   } else{
-  #     plot_final = NULL
-  #   }
-  #   return(plot_final)
-  # })
-  
-  
-  pt1 <- reactive({
-    if (!input$check1) return(NULL)
+  ######################################################################
+  # Sick
+  ######################################################################
+  plot_sick_1 <- reactive({
+    if (!input$check_sick_1) return(NULL)
+    date_range <- c(input$dates_sick[1], input$dates_sick[2])
     dataset<-data.frame(data$casos,data$fecha)
     colnames(dataset)[1] <- "values"
     colnames(dataset)[2] <- "dates"
-    title<-"Total cases vs date"
+    title<-"Sick vs date"
     ylab<-"Cases (linear)"
     xlab<-"Date"
-    ggplotly(plot_total_linear_func(dataset,title,ylab,xlab))
+    ggplotly(plot_total_linear_func(date_range,dataset,title,ylab,xlab))
       })
-  pt2 <- reactive({
-    if (!input$check2) return(NULL)
+  plot_sick_2 <- reactive({
+    if (!input$check_sick_2) return(NULL)
+    date_range <- c(input$dates_sick[1], input$dates_sick[2])
     dataset<-data.frame(data$casos,data$fecha)
     colnames(dataset)[1] <- "values"
     colnames(dataset)[2] <- "dates"
-    title<-"Total cases vs date"
+    title<-"Sick vs date"
     ylab<-"Cases (log)"
     xlab<-"Date"
-    ggplotly(plot_total_log_func(dataset,title,ylab,xlab))
+    ggplotly(plot_total_log_func(date_range,dataset,title,ylab,xlab))
     })
-  pt3 <- reactive({
-    if (!input$check3) return(NULL)
+  plot_sick_3 <- reactive({
+    if (!input$check_sick_3) return(NULL)
+    date_range <- c(input$dates_sick[1], input$dates_sick[2])
     dataset<-data.frame(data$casos,data$fecha)
     colnames(dataset)[1] <- "values"
     colnames(dataset)[2] <- "dates"
-    title<-"New cases vs date"
+    title<-"Sick vs date"
     ylab<-"New cases (absolute)"
     xlab<-"Date"
-    ggplotly(plot_new_cases_abs_func(dataset,title,ylab,xlab))
+    ggplotly(plot_new_cases_abs_func(date_range,dataset,title,ylab,xlab))
   })
-  pt4 <- reactive({
-    if (!input$check4) return(NULL)
+  plot_sick_4 <- reactive({
+    if (!input$check_sick_4) return(NULL)
+    date_range <- c(input$dates_sick[1], input$dates_sick[2])
     dataset<-data.frame(data$casos,data$fecha)
     colnames(dataset)[1] <- "values"
     colnames(dataset)[2] <- "dates"
-    title<-"New cases vs date"
+    title<-"Sick vs date"
     ylab<-"New cases (+%)"
     xlab<-"Date"
-    ggplotly(plot_new_cases_perc_func(dataset,title,ylab,xlab))
+    ggplotly(plot_new_cases_perc_func(date_range,dataset,title,ylab,xlab))
     })
   
   output$plot_sick = renderPlotly({
-    ptlist <- list(pt1(),pt2(),pt3(),pt4())
+    ptlist <- list(plot_sick_1(),plot_sick_2(),plot_sick_3(),plot_sick_4())
     if (length(ptlist)==1){
       wtlist=c(100)
     } 
@@ -257,6 +245,77 @@ server <- function(input, output) {
     if (length(ptlist)==0) return(NULL)
     return(subplot(ptlist, nrows=length(ptlist),shareX = T, shareY = F))
     })
+  
+  
+  ######################################################################
+  # Dead
+  ######################################################################
+  plot_dead_1 <- reactive({
+    if (!input$check_dead_1) return(NULL)
+    date_range <- c(input$dates_dead[1], input$dates_dead[2])
+    dataset<-data.frame(data$fallecimientos,data$fecha)
+    colnames(dataset)[1] <- "values"
+    colnames(dataset)[2] <- "dates"
+    title<-"Dead vs date"
+    ylab<-"Dead (linear)"
+    xlab<-"Date"
+    ggplotly(plot_total_linear_func(date_range,dataset,title,ylab,xlab))
+  })
+  plot_dead_2 <- reactive({
+    if (!input$check_dead_2) return(NULL)
+    date_range <- c(input$dates_dead[1], input$dates_dead[2])
+    dataset<-data.frame(data$fallecimientos,data$fecha)
+    colnames(dataset)[1] <- "values"
+    colnames(dataset)[2] <- "dates"
+    title<-"Dead vs date"
+    ylab<-"Dead (log)"
+    xlab<-"Date"
+    ggplotly(plot_total_log_func(date_range,dataset,title,ylab,xlab))
+  })
+  plot_dead_3 <- reactive({
+    if (!input$check_dead_3) return(NULL)
+    date_range <- c(input$dates_dead[1], input$dates_dead[2])
+    dataset<-data.frame(data$fallecimientos,data$fecha)
+    colnames(dataset)[1] <- "values"
+    colnames(dataset)[2] <- "dates"
+    title<-"Dead vs date"
+    ylab<-"New dead (absolute)"
+    xlab<-"Date"
+    ggplotly(plot_new_cases_abs_func(date_range,dataset,title,ylab,xlab))
+  })
+  plot_dead_4 <- reactive({
+    if (!input$check_dead_4) return(NULL)
+    date_range <- c(input$dates_dead[1], input$dates_dead[2])
+    dataset<-data.frame(data$fallecimientos,data$fecha)
+    colnames(dataset)[1] <- "values"
+    colnames(dataset)[2] <- "dates"
+    title<-"Dead vs date"
+    ylab<-"New dead (+%)"
+    xlab<-"Date"
+    ggplotly(plot_new_cases_perc_func(date_range,dataset,title,ylab,xlab))
+  })
+  
+  output$plot_dead = renderPlotly({
+    ptlist <- list(plot_dead_1(),plot_dead_2(),plot_dead_3(),plot_dead_4())
+    if (length(ptlist)==1){
+      wtlist=c(100)
+    } 
+    if (length(ptlist)==2){
+      wtlist=c(50,50)
+    } 
+    if (length(ptlist)==3){
+      wtlist=c(33.33,33.33,33.33)
+    }
+    if (length(ptlist)==4){
+      wtlist=c(25,25,25,25)
+    } 
+    # remove the null plots from ptlist and wtlist
+    to_delete <- !sapply(ptlist,is.null)
+    ptlist <- ptlist[to_delete] 
+    wtlist <- wtlist[to_delete]
+    if (length(ptlist)==0) return(NULL)
+    return(subplot(ptlist, nrows=length(ptlist),shareX = T, shareY = F))
+  })
   
 }
 
